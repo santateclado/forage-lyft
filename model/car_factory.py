bc5eeb83047d8e3eccdb0f82ca203b12a6fd8b4c
Jason Martinez <martinez.jas@northeastern.edu>
2023-02-15 22:07:01 -0500

    assigned tires to each model

--- model/car_factory.py
+++ model/car_factory.py
@@ -1,5 +1,6 @@
 from battery import nubbin_battery, spindler_battery
 from engine import capulet_engine, willoughby_engine, sternman_engine
+from tires import carrigan_tires, octoprime_tires
 from objects import Car
 
 
@@ -7,43 +8,48 @@ class CarFactory:
     """A factory for cars"""
 
     @classmethod
-    def create_calliope(cls, last_service_date, current_mileage, last_service_mileage):
+    def create_calliope(cls, last_service_date, current_mileage, last_service_mileage, tire_status):
         """Creates a Calliope car"""
         engine = capulet_engine.__init__(last_service_mileage, current_mileage)
         battery = spindler_battery.__init__(last_service_date)
-        car = Car.__init__(engine, battery)
+        tires = carrigan_tires.__init__(tire_status)
+        car = Car.__init__(engine, battery, tires)
         return car
 
     @classmethod
-    def create_glissade(cls, last_service_date, current_mileage, last_service_mileage):
+    def create_glissade(cls, last_service_date, current_mileage, last_service_mileage, tire_status):
         """Creates a Glissade car"""
         engine = willoughby_engine.__init__(
             last_service_mileage, current_mileage)
         battery = spindler_battery.__init__(last_service_date)
-        car = Car.__init__(engine, battery)
+        tires = carrigan_tires.__init__(tire_status)
+        car = Car.__init__(engine, battery, tires)
         return car
 
     @classmethod
-    def create_palindrome(cls, last_service_date, warning_light_on):
+    def create_palindrome(cls, last_service_date, warning_light_on, tire_status):
         """Creates a Palindrome car"""
         engine = sternman_engine.__init__(warning_light_on)
         battery = spindler_battery.__init__(last_service_date)
-        car = Car.__init__(engine, battery)
+        tires = octoprime_tires.__init__(tire_status)
+        car = Car.__init__(engine, battery, tires)
         return car
 
     @classmethod
-    def create_rorschach(cls, last_service_date, current_mileage, last_service_mileage):
+    def create_rorschach(cls, last_service_date, current_mileage, last_service_mileage, tire_status):
         """Creates a Rorschach car"""
         engine = willoughby_engine.__init__(
             last_service_mileage, current_mileage)
         battery = nubbin_battery.__init__(last_service_date)
-        car = Car.__init__(engine, battery)
+        tires = octoprime_tires.__init__(tire_status)
+        car = Car.__init__(engine, battery, tires)
         return car
 
     @classmethod
-    def create_thovex(cls, last_service_date, current_mileage, last_service_mileage):
+    def create_thovex(cls, last_service_date, current_mileage, last_service_mileage, tire_status):
         """Creates a Thovex car"""
         engine = capulet_engine.__init__(last_service_mileage, current_mileage)
         battery = nubbin_battery.__init__(last_service_date)
-        car = Car.__init__(engine, battery)
+        tires = carrigan_tires.__init__(tire_status)
+        car = Car.__init__(engine, battery, tires)
         return car
